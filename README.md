844befaf093d1acc88ac9ad757aa64889afee652
manuelguarniz <cruzemg95@gmail.com>
2016-10-05 18:04:50 -0500

    README.me -> Modificando error en extension, agregando mas detallado al documento

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 Se solicita poder gestionar las operaciones basicas para un trabajador, donde se necesitara su nombre, apellidos, edad y un cargo a asignar en la empresa usando un `<select><option>CARGO</option></select>`.
 El trabajador no podra cambiar de cargo desde la opcion de modificar (editar)
 
+### Requisitos necesarios:
+- Ejecutar script sql en su gestor de datos favorito (personalmente uso MySQL)
+link: [Script de la base de datos](https://github.com/OneSevenDev/demoSpring/blob/master/databaseempresa.sql)
+- Agregar datos a la base de datos para poder visualizar la lista
+- Instalar la extension de [Spring STS](https://spring.io/) en [Eclipse](https://eclipse.org/downloads/)
+  - Link donde explico que extension descargar: [Link de tutorial Hola mundo Spring](https://manuelguarnizblog.wordpress.com/2016/09/28/hola-mundo-con-spring-mvc/)
+- Usar el codigo como mejor sea, cualquier recomendación visitenme en [@Cruzemg95](https://twitter.com/cruzemg95)
+
 ### Imagenes demostrativas del CRUD en Spring MVC
 
 ** Imagen demostrativa de CREATE **
--- databaseempresa.sql
+++ databaseempresa.sql
@@ -11,26 +11,3 @@ CREATE TABLE cargo(
 	id int(11) auto_increment PRIMARY KEY,
     nombrecargo varchar(45)
 );
-INSERT INTO cargo(nombrecargo)
-VALUES ('Administrador'), ('Secretaria'),('Operario');
-INSERT INTO trabajador(nombres,apellidos,edad,id_cargo)
-VALUES ('Manuel','Guarniz',21,1),('Mario','Torres',24,3),('Dana','Matiel',19,2);
-SELECT * FROM cargo;
-SELECT t.id,nombres,apellidos,edad,c.nombrecargo FROM trabajador t INNER JOIN cargo c ON t.id_cargo = c.id;
-USE `empresa`;
-DROP procedure IF EXISTS `spListarTrabajadores`;
-
-DELIMITER $$
-USE `empresa`$$
-CREATE PROCEDURE `spListarTrabajadores` ()
-BEGIN
-	SELECT t.id,nombres,apellidos,edad,c.nombrecargo FROM trabajador t INNER JOIN cargo c ON t.id_cargo = c.id;
-END$$
-
-DELIMITER ;
-
-CALL spListarTrabajadores();
-SELECT id, nombrecargo FROM cargo ORDER BY nombrecargo;
-DELETE FROM trabajador WHERE id = 4;
-UPDATE trabajador SET nombres='marita',apellidos='lujan',edad=18, id_cargo = 3 WHERE id = 7;
-SELECT t.id,nombres,apellidos,edad,c.id,c.nombrecargo FROM trabajador t INNER JOIN cargo c ON t.id_cargo = c.id WHERE t.id = 9;
